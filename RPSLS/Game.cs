50cd5662dd3bc4a17737fbeac746cc571c8c3b79
IseTrade <IseTrade@gmail.com>
2018-09-22 02:43:44 -0500

    Some innitial codes for program.  Game engine is working but still need a lot of work in organizing classes, groups, variables.

---
+++ RPSLS/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Game
+    {
+        public void Engine()
+        {
+            Players player1 = new Players();
+            // test1.Choice1();
+            Players player2 = new Players();
+            // test2.Choice2();
+
+            int p1; int p2;
+            bool b1; bool b2; bool b3; bool b4; bool b5;
+
+            p1 = 4; //test overrides
+            p2 = 2; //test overrides
+
+            //boolean conditions must hold a boolean (mini inline if statement -- TERNARY OPERATOR)
+            b1 = (p1 == 1 && (p2 == 3 || p2 == 4)); // ? true : false;
+            b2 = (p1 == 2 && (p2 == 1 || p2 == 5)); // ? true : false;
+            b3 = (p1 == 3 && (p2 == 2 || p2 == 4)); // ? true : false;
+            b4 = (p1 == 4 && (p2 == 5 || p2 == 2)); // ? true : false;
+            b5 = (p1 == 5 && (p2 == 3 || p2 == 1)); // ? true : false;
+
+            if (p1 == p2)
+            {
+                Console.WriteLine("It's a draw.");
+                Console.ReadKey();
+            }
+            else if ((b1 || b2 || b3 || b4 || b5) == true)
+            {
+                Console.WriteLine("Congrats player 1. YOU WIN!!!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Congrats player 2. YOU WIN!!!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ RPSLS/Players.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Players
+    {
+        public void Gesture()
+        {
+            Console.WriteLine("Please enter your choice: ");
+            Console.WriteLine("1.   Rock");
+            Console.WriteLine("2.   Paper");
+            Console.WriteLine("3.   Scissors");
+            Console.WriteLine("4.   Lizard");
+            Console.WriteLine("5.   Spock");
+            //string choice1 = Console.ReadLine();
+            string gesture = "1"; //test
+
+            switch (gesture)
+            {
+                case "1":
+                    Console.WriteLine("You've selected Rock");
+                    //Console.ReadKey();
+                    break;
+                case "2":
+                    Console.WriteLine("You've selected Paper");
+                    //Console.ReadKey();
+                    break;
+                case "3":
+                    Console.WriteLine("You've selected scissors");
+                    //Console.ReadKey();
+                    break;
+                case "4":
+                    Console.WriteLine("You've selected Lizard");
+                    //Console.ReadKey();
+                    break;
+                case "5":
+                    Console.WriteLine("You've selected Spock");
+                    //Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid number!!!");
+                    //Console.ReadKey();
+                    break;
+            }
+        }
+
+
+
+
+
+    }
+}
